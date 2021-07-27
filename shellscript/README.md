5a18a530d2069e7ec0a64158bc96da2244ac522a
Ryan Taylor <ryan.d.taylor@computecanada.ca>
2021-07-27 08:11:14 -0400

    Added some comments to document use of shell script

---
+++ shellscript/README.md
@@ -0,0 +1,13 @@
+On a fresh Ubuntu VM, you can use this shell script to install
+Atlascine using these instructions:
+
+```bash
+git clone https://github.com/ResearchComputingServices/nunaliit-devops.git
+
+cd nunaliit-devops/shellscript/
+
+cp config_EXAMPLE.sh config.sh
+# EDIT config.sh as needed
+
+sudo -s ./install-atlascine.sh
+```
--- shellscript/config_EXAMPLE.sh
+++ shellscript/config_EXAMPLE.sh
@@ -1,36 +1,28 @@
-############################################################
-#
-# CONFIGURATION FOR INSTALL ATLASCINE
-#
-# config_EXAMPLE.sh shows what settings you can put in your own
-# config.sh.  You may need to customize some settings for your
-# environment.
-#
-############################################################
+#############################################################
+##
+## CONFIGURATION FOR INSTALL ATLASCINE
+##
+## config_EXAMPLE.sh shows what settings you can put in your own
+## config.sh.  You will need to customize some settings for your
+## environment.
+##
+## You should pay particular attention to the
+## following settings:
+##   - USER_BUILD_NUNALIIT
+##   - GCRC_GITLAB_USER/GCRC_GITLAB_PASS
+##   - COUCHDB_PASS
+##   - DB_NAME/DB_SUBMISSION_NAME
+##   - ATLAS_NAME
+##   - ATLAS_TEMPLATE_BRANCH
+##   - ATLAS_FOLDER
+#############################################################
 
-COUCHDB_PASS="your_password_here"
-NUNALIIT_BRANCH="branch-for-atlascine"
-ATLAS_TEMPLATE_BRANCH="rwanda"
 
-BASE_FOLDER="/atlascine"
-ATLAS_FOLDER="$BASE_FOLDER/rwanda"
-SOURCE_FOLDER="$BASE_FOLDER/nunaliit_source"
-
-ATLAS_NAME="rwanda"
-DB_NAME="rwanda"
-DB_SUBMISSION_NAME="rwandasubmissions"
-
-RESTORE_EXISTING_DB=1
-DUMP_FOLDER="$BASE_FOLDER/dumps"
-# Restore this dump if RESTORE_EXISTING_DB=1
-DUMP_FILE="/vagrant/rwanda-atlas_dump_2021-03-10.tar.gz"
-UNPACKED_DUMP_NAME=dump_2021-03-10_16:37:24
-
-# Optionally put nunaliit command in a convenient location (e.g on
-# search PATH) to make it easy to run from the command-line
-USE_BINDIR=1
-BINDIR="/usr/local/bin"
-
+##############################################
+##
+## UBUNTU USER ACCOUNT
+##
+##############################################
 # mvn install for the Nunaliit build cannot run as root.  I believe it
 # is due to this bug:
 #    https://github.com/npm/cli/issues/624
@@ -38,5 +30,57 @@ BINDIR="/usr/local/bin"
 # Change to some appropriate account on your system:
 USER_BUILD_NUNALIIT="ubuntu"
 
-GCRC_GITLAB_USER=""
-GCRC_GITLAB_PASS=""
+
+##############################################
+##
+## NUNALIIT SOURCE CODE
+##
+##############################################
+GCRC_GITLAB_USER=""                                                 # User account at https://github.com/GCRC, to download Nunaliit
+GCRC_GITLAB_PASS=""                                                 # Corresponding password at https://github.com/GCRC
+NUNALIIT_BRANCH="branch-for-atlascine"                              # Branch of Nunaliit to use for Atlascine install
+
+
+##############################################
+##
+## COUCHDB DATABASE
+##
+##############################################
+COUCHDB_PASS="your_password_here"                                   # Will set couchdb and your Atlas to use this db password
+DB_NAME="rwanda"                                                    # Database name for your Atlas
+DB_SUBMISSION_NAME="rwandasubmissions"                              # Submission database name
+
+
+##############################################
+##
+## ATLAS SETTINGS
+##
+##############################################
+ATLAS_NAME="rwanda"                                                 # Name to call this Atlas installation
+ATLAS_TEMPLATE_BRANCH="rwanda"                                      # gitlab atlas template branch to use for this Atlas
+
+
+##############################################
+##
+## TARGET DIRECTORIES
+##
+##############################################
+BASE_FOLDER="/atlascine"                                            # I put everything in here
+ATLAS_FOLDER="$BASE_FOLDER/rwanda"                                  # Location to install atlas
+SOURCE_FOLDER="$BASE_FOLDER/nunaliit_source"                        # location to download nunaliit source code
+DUMP_FOLDER="$BASE_FOLDER/dumps"                                    # If you restor a dump, it will be unpacked in here
+BINDIR="/usr/local/bin"                                             # Put a shortcut to nunaliit command here, so it is easier to run
+
+
+##############################################
+##
+## RESTORE EXISTING ATLAS
+##
+##############################################
+#
+# If you have a dump file from an existing atlas, you can
+# use the following settings to restore it
+RESTORE_EXISTING_DB=0                                               # 0=skip atlas restore   1=do atlas restore
+# Restore this dump if RESTORE_EXISTING_DB=1
+DUMP_FILE="/home/ubuntu/rwanda-atlas_dump_2021-03-10.tar.gz"        # If restore enable, restore from this dump file
+UNPACKED_DUMP_NAME="dump_2021-03-10_16:37:24"                       # Name of folder inside your dump file
